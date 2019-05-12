eccda5def1c10ea25a8c3d663a6fd746e3ea408a
AJay Marathe <0107amar@gmail.com>
2019-05-13 01:55:15 +0530

    created post, crate-post, edit-post, show-post component with services, and added angular material

---
+++ src/app/show-post/show-post.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-post',
+  templateUrl: './show-post.component.html',
+  styleUrls: ['./show-post.component.css']
+})
+export class ShowPostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
